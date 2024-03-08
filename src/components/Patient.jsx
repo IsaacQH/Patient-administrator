80985c9b6a57dfc246c6170cbe4703d5ace3ed5a
IsaacQH <isaacqwert@gmail.com>
2024-03-08 00:04:47 -0600

    Adding delete and edit buttons, only visual not functional yet

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -6,7 +6,7 @@ const Patient = ({patient}) => {
 
     return (
     
-    <div className="m-3 bg-indigo-700 shadow-2xl rounded-lg px-5 py-10">
+    <div className="m-3 bg-indigo-700 shadow-2xl rounded-lg px-5 py-6">
         <p className="font-bold mb-3 text-white uppercase">Name: 
         <span className="font-normal normal-case text-white"> {nombre}</span>
         </p>
@@ -23,6 +23,18 @@ const Patient = ({patient}) => {
         <p className="font-bold mb-3 text-white uppercase">Symptoms: 
         <span className="font-normal normal-case  text-white"> {symptoms}</span>
         </p>
+
+        <div className="mt-6 flex justify-end">
+            <button
+                type="button"
+                className="py-2 px-10 bg-indigo-400 hover:bg-indigo-500 text-white font-bold uppercase rounded-lg"
+            >Edit</button>
+                        <button
+                type="button"
+                className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg ml-3"
+            >Delete</button>
+        </div>
+
     </div>
   )
 }
