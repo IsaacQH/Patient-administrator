6e138d0ec7d8067745641eed73c1162944f5d83a
IsaacQH <isaacqwert@gmail.com>
2024-03-12 13:07:14 -0600

    Returning original:!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         <>
             <h1 className="font-black text-5xl text-center md:w-1/3 mx-auto"> {/*Le da un fondo negro, un tamaño de 5xl y lo centra, Le da margen right de 10 y width de 1/2 (33%), el md da un val minimo*/}
                 Patient Tracking
-                <span className=" text-red-700"> Veterinary</span>
+                <span className=" text-indigo-700"> Veterinary</span>
             </h1>
         </>
     )
